f8eb85f440d3d00bc4b0f9554acd82e326881053
ToXer04 <simonefarina2004@gmail.com>
2024-01-27 19:27:16 +0100

    Completed H2 webapp

---
+++ webapp/src/main/java/h2/example/webapp/entities/Student.java
@@ -0,0 +1,23 @@
+package h2.example.webapp.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "students")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false, unique = true)
+    private String email;
+}
